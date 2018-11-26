dbdee70bf938e09b94409f5ac04f8d923b849e2e
Jan Kowalski <jan@o2.pl>
2018-11-26 17:17:20 +0100

    kolokwium

---
+++ navbar.js
@@ -0,0 +1,9 @@
+
+let nav = '<nav>\n' +
+    '<h1>Nawigacja</h1>' +
+    '<li><a href="index.html">Strona główna</a></li>' +
+    '<li><a href="kontakt.html">Kontakt</a></li>'+
+    '</nav>';
+
+let navBar = document.querySelector(".nav-bar");
+navBar.innerHTML=nav;
